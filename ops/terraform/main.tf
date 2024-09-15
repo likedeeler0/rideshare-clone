fb983c900a6d62e70d48ca858f19669db03aa93b
ethanj12 <ethanjones0624@gmail.com>
2024-09-15 15:35:37 -0700

    ?

--- ops/terraform/main.tf
+++ ops/terraform/main.tf
@@ -8,7 +8,7 @@ resource "aws_s3_bucket" "tfstate" {
 
 resource "aws_db_instance" "driver_locations_polled_10sec" {
   allocated_storage    = 10
-  identifier = "driver-locations-polled-10sec"
+  identifier           = "driver-locations-polled-10sec"
   db_name              = "driver_locations_polled_10sec"
   engine               = "postgres"
   engine_version       = "16.3"
